bec53055b82141cda1c26189c6a3b9fb15cba773
choho1515 <65482819+choho1515@users.noreply.github.com>
2020-05-26 13:39:35 +0900

    Update GoodBap.js

--- GoodBap.js
+++ GoodBap.js
@@ -1,28 +1,19 @@
-let bap = [9000, 7000, 6000, 5000]; //국밥 가격들
-function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-    if (msg.startsWith("!국밥 ")) {
-        let ms = msg.substr(4).split(" ")
-        //↓숫자인지 아닌지
-        if (!isNaN(ms)) {
-            if (ms <= average(bap)) {
-                replier.reply("평균 가격인 " + numberWithCommas(average(bap)) + "원보다\n" + numberWithCommas(Math.floor(average(bap) - ms)) + "원 부족해, 즉 국밥을 살수 없어.");
-            } else
-            if (ms >= 1000000000000) {
-                replier.reply("정상적인 가격을 넣어.");
-            } else replier.reply("그걸 살 바에 " + numberWithCommas(Math.floor(ms / average(bap))) + "개의 국밥을\n뜨ㅡㅡ끈하고 든ㅡ든하게 먹는게 낫지.");
-        } else replier.reply("숫자만 넣어.");
+let bap = [9e3, 7e3, 6e3, 5e3];
+
+function response(e, r, a, t, n, o, i) {
+    if (r.startsWith("!국밥 ")) {
+        let e = r.substr(4).split(" ");
+        isNaN(e) ? n.reply("숫자만 넣어.") : e <= average(bap) ? n.reply("평균 가격인 " + numberWithCommas(average(bap)) + "원보다\n" + numberWithCommas(Math.floor(average(bap) - e)) + "원 부족해, 즉 국밥을 살수 없어.") : e >= 1e12 ? n.reply("정상적인 가격을 넣어.") : n.reply("그걸 살 바에 " + numberWithCommas(Math.floor(e / average(bap))) + "개의 국밥을\n뜨ㅡㅡ끈하고 든ㅡ든하게 먹는게 낫지.")
     }
 }
 
-function average(array) {
-    let result = 0;
-    let arrLength = array.length;
-    for (var i = 0; i < arrLength; i++) {
-        result += array[i];
-    }
-    return result / arrLength;
+function average(e) {
+    let r = 0,
+        a = e.length;
+    for (var t = 0; t < a; t++) r += e[t];
+    return r / a
 }
 
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+function numberWithCommas(e) {
+    return e.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 }
